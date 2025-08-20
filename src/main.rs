e8941fac4736ad62099307f64a523ced33859801
Jacob Asper <jacobasper191@gmail.com>
2025-08-20 00:36:43 -0400

    Final version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use core::ops::RangeInclusive;
 use std::fs;
-use std::path::Path;
-use std::process::Command;
+use std::path::PathBuf;
 
 // should be 53 at max
-const POWER: i64 = 10;
+const POWER: i64 = 1;
 const JS_MAX_SAFE_INTEGER: i64 = (1 << POWER) - 1;
 const JS_MIN_SAFE_INTEGER: i64 = -JS_MAX_SAFE_INTEGER;
 const JS_SAFE_INTEGER_RANGE: RangeInclusive<i64> = JS_MIN_SAFE_INTEGER..=JS_MAX_SAFE_INTEGER;
@@ -36,13 +35,21 @@ fn human_size(bytes: u64) -> String {
 fn main() {
     println!("Generating file for {:?}", JS_SAFE_INTEGER_RANGE);
 
-    let output = build(JS_SAFE_INTEGER_RANGE);
+    let out_file = PathBuf::from("ts-playground/safe-integer.ts");
 
-    let out_file = Path::new("ts-playground").join("safe-integer.ts");
-    if let Err(e) = fs::write(&out_file, output) {
-        eprintln!("Failed to write {}: {e}", out_file.display());
-        std::process::exit(1);
-    }
+    let res = {
+        let ts_union = JS_SAFE_INTEGER_RANGE
+            .map(|x| format!("|{}", x))
+            .collect::<String>();
+        format!(
+            "type SafeInteger = {ts_union};
+const y: SafeInteger = 0;
+// @ts-expect-error
+const X: SafeInteger = Infinity;",
+        )
+    };
+
+    fs::write(&out_file, res).unwrap();
 
     println!("Generated {}", out_file.display());
     let size_bytes = fs::metadata(&out_file).map(|m| m.len()).unwrap();
@@ -51,36 +58,4 @@ fn main() {
         human_size(size_bytes),
         size_bytes
     );
-
-    // run tsc to check the generated file
-    let res = Command::new("npm")
-        .arg("run")
-        .arg("typecheck")
-        .current_dir("ts-playground")
-        .output()
-        .unwrap();
-
-    println!("{}", String::from_utf8(res.stdout).unwrap());
-    eprintln!("{}", String::from_utf8(res.stderr).unwrap());
-
-    if !res.status.success() {
-        eprintln!("TypeCheck failed");
-        std::process::exit(1);
-    }
-    println!("TypeCheck passed gaming for {:?}", JS_SAFE_INTEGER_RANGE);
-}
-
-fn union_chunk(x: i64) -> String {
-    format!("\t| {}", x)
-}
-fn build(range: RangeInclusive<i64>) -> String {
-    let union = range.map(union_chunk).collect::<Vec<_>>().join("\n");
-    format!(
-        "type SafeInteger =
-{union};
-
-const y: SafeInteger = 5;
-// @ts-expect-error
-const X: SafeInteger = Infinity;",
-    )
 }
